d43cefc1d93f5ccfc041a2a10ec36ef2ba467c0c
Klaas de Vries <klaasjacobdevries@gmail.com>
2019-08-27 22:42:58 +0100

    fixing

--- {{cookiecutter.name}}/lib/sample.cc
+++ {{cookiecutter.name}}/lib/sample.cc
@@ -3,14 +3,8 @@
 
 namespace {{cookiecutter.namespace}} {
 
-namespace {
-results::option<std::string> hello() {
-  return results::make_some<std::string>("hello from a ");
-}
-}
-
 std::string public_function() {
-  return hello().map([](auto s) { return s + "public function"; }).unwrap();
+  return "hello from a public function";
 }
 
 } // namespace {{cookiecutter.namespace}}
